f4f5659ce6cac1e619bf23bca67a15f4813277ba
Jose Carlos Couto <dsilvacouto5@gmail.com>
2025-08-04 15:50:31 +0100

    Update cookingClassPage.ts

--- src/pages/cookingClassPage.ts
+++ src/pages/cookingClassPage.ts
@@ -1,13 +1,29 @@
 import { Page } from '@playwright/test';
 
 export class CookingClassPage {
-  constructor(private page: Page) {}
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
 
   async navigateToClass(classId: string) {
+    // Navigates to a specific cooking class page using the class ID
     await this.page.goto(`/cooking-classes/${classId}`);
   }
 
-  async getLocation() {
+  async getLocation(): Promise<string> {
+    // Retrieves the location text from the page
     return await this.page.innerText('.location');
+  }
+
+  async getTitle(): Promise<string> {
+    // Retrieves the title of the cooking class
+    return await this.page.innerText('.title');
+  }
+
+  async getDescription(): Promise<string> {
+    // Retrieves the description of the cooking class
+    return await this.page.innerText('.description');
   }
 }
